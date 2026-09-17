d90699c1e088b403d7457fe0078ad360295ca7e8
Jhojan <jhojan.mina@inacapmail.cl>
2026-09-16 22:52:41 -0300

    Create urls.py

---
+++ django-shop/config/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from shop import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('categoria/<str:categoria>/', views.categoria, name='categoria')
+]
